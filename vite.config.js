147b67e0846b95774b30283c7fbe0a96f19fbd05
Alexis Garcia <alexisgarciagarcia@alumno.ieselrincon.es>
2024-12-10 20:03:51 +0000

    render 1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //agregado para render
+  build: {
+    outDir: 'dist', // Asegúrate de que esté configurado como 'dist'
+  },
 })
