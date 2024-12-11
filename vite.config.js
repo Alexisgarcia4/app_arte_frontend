dfa660a17f653f01d7f4352726cf15260416b304
Alexis Garcia <alexisgarciagarcia@alumno.ieselrincon.es>
2024-12-11 13:49:41 +0000

    render 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',          // Expone el servidor a todas las interfaces de red
+    port: process.env.PORT || 3000  // Usa el puerto proporcionado por Render o 3000 por defecto
+  }
   
 })
